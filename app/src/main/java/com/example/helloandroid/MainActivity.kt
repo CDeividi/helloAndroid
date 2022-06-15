caa6b7429a4de7d180d226fda1c50fcbc37b972f
CDeividi <89434806+CDeividi@users.noreply.github.com>
2022-06-14 21:08:10 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //teste1
+//teste push
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
